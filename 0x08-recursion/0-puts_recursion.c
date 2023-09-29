7f9be2bfaa2b5d1ba2bc7938d53ecf70e4f9081a
Desalegn Wondmu <sylblade1@gmail.com>
2023-09-29 02:27:28 -0700

    added puts recursion, readme and main.h

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string followed by a new line
+ *@str: input string
+ * return: string to be printed
+ */
+
+void _puts_recursion(char *str)
+{
+	if (*str == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	putchar(*str + 1);
+	_puts_recursion(str + 1);
+}
+
