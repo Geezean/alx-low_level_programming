e314cec35c36a66cd48eed7b8de32ad7dc28d091
Desalegn Wondmu <sylblade1@gmail.com>
2023-10-02 17:36:32 -0700

    created args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** main - prints the number of arguments passed onto the program.
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always (0) success.
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
