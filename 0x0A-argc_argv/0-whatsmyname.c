4de3b0599c55c1718eed37f7f3403ecf3f6ad943
Desalegn Wondmu <sylblade1@gmail.com>
2023-10-02 17:18:52 -0700

    added whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Its own name, followed by a new line.
+ * @argc: Argument count
+ * @argv: Argument vectors
+ * Return: 0 on Success.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
